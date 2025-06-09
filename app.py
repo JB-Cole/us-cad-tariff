df403202ee15acc10606e20de5934bb059a51979
James Cole <jamescole@Jamess-MacBook-Pro-2.local>
2025-06-09 02:09:50 +0000

    update app.py

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ if st.button("Clear Cache"):
 
 st.title('🏗️ U.S. - Canada Tariff Impact on Construction and Production sectors')
 st.markdown("""
-Welcome to an interactive dashboard for exploring the impact of U.S. - Canada tariffs on Canada's production and construction sectors. On the Trends page, you will find a button to retrieve IPPI data for Fabricated metal products and construction materials [P63] and RMPI for Metal ores, concentrates and scrap [M61] both from Statistics Canada. You can filter these indeces by date to highlight two key phases of the tariff dispute: the tariff implementation (January 2025) and the subsequent lobbying period (March 2025). Since StatCan updates its data quarterly, the most recent figures extend through April 2025, and you can visualize indices dating back to 2020. All StatCan data is scraped and saved as CSV files in the Data tab.
+Welcome to an interactive dashboard for exploring the impact of U.S. - Canada tariffs on Canada's production and construction sectors.
+
+On the Trends page, you will find a button to retrieve IPPI data for Fabricated metal products and construction materials [P63] and RMPI for Metal ores, concentrates and scrap [M61] both from Statistics Canada.
+
+You can filter these indeces by date to highlight two key phases of the tariff dispute: the tariff implementation (January 2025) and the subsequent lobbying period (March 2025). 
+
+Since StatCan updates its data quarterly, the most recent figures extend through April 2025, and you can visualize indices dating back to 2020. All StatCan data is scraped and saved as CSV files in the Data tab.
 
 Use the sidebar to switch between pages:
 - Data 
